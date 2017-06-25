33d89bebbb84a5c2d220386278044d1d3d670907
Christopher Jenness <christopherjenness@gmail.com>
2017-06-25 10:57:29 -0400

    Change to main acount

--- lib/settings.py
+++ lib/settings.py
@@ -1,5 +1,5 @@
 import os
 
 MARKET_ADDRESS = "18VJ5dy5YR6bK8H5EFb2o6dFz3pKaze8Ns"
-ACCOUNT_NAME = "SECONDARY-ACCOUNT"
+ACCOUNT_NAME = "MAIN-ACCOUNT"
 user_dir = os.path.expanduser('~') + "/.book-lib/"
